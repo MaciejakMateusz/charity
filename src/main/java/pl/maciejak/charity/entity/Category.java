755cd19c3efcb40003e908eec876f040b25d2bdf
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-09-19 20:38:50 +0200

    Created Category entity

---
+++ src/main/java/pl/maciejak/charity/entity/Category.java
@@ -0,0 +1,19 @@
+package pl.maciejak.charity.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Table(name = "categories")
+@Entity
+@Getter
+@Setter
+@ToString
+public class Category {
+    @Id
+    private Integer id;
+    private String name;
+}
