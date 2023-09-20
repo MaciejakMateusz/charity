db914aefe1a6395938795c01e0c340a5713b2aa3
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-09-20 16:41:24 +0200

    Added counting method to DonationRepository

--- src/main/java/pl/maciejak/charity/repository/DonationRepository.java
+++ src/main/java/pl/maciejak/charity/repository/DonationRepository.java
@@ -1,7 +1,10 @@
 package pl.maciejak.charity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.maciejak.charity.entity.Donation;
 
 public interface DonationRepository extends JpaRepository<Donation, Integer> {
+    @Query(value = "SELECT COUNT(*) FROM charity_donation.dontations", nativeQuery = true)
+    Integer countAll();
 }
