aaa70a60fd82c242d697c46f16384926433ae12c
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-09-20 11:21:25 +0200

    Created UserRepository

---
+++ src/main/java/pl/maciejak/charity/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package pl.maciejak.charity.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.maciejak.charity.entity.User;
+
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findUserByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+}
