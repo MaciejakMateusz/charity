d2a60847d1b80b986e689f6e22c8915fb759dc23
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-09-20 16:21:33 +0200

    Created getInstitutions() @ModelAttribute method in HomeController to pass list of all foundations to "/" mapped views

--- src/main/java/pl/maciejak/charity/controller/HomeController.java
+++ src/main/java/pl/maciejak/charity/controller/HomeController.java
@@ -2,14 +2,30 @@ package pl.maciejak.charity.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.maciejak.charity.entity.Institution;
+import pl.maciejak.charity.repository.InstitutionRepository;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+    private final InstitutionRepository institutionRepository;
+
+    public HomeController(InstitutionRepository institutionRepository) {
+        this.institutionRepository = institutionRepository;
+    }
+
     @GetMapping
     public String home() {
         return "home";
+    }
+
+    @ModelAttribute("institutions")
+    private List<Institution> getInstitutions() {
+        return institutionRepository.findAll();
     }
 
 }
