b93d44c2e5ba1e2f20c7b718902649fed849c12d
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-09-20 11:38:07 +0200

    Created separate controller to handle user registration

---
+++ src/main/java/pl/maciejak/charity/controller/RegisterController.java
@@ -0,0 +1,45 @@
+package pl.maciejak.charity.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.maciejak.charity.entity.User;
+import pl.maciejak.charity.service.UserService;
+
+@Controller
+@RequestMapping("/register")
+public class RegisterController {
+
+    private final UserService userService;
+
+    public RegisterController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String register(Model model) {
+        return "register";
+    }
+
+    @PostMapping
+    public String register(@Valid User user, BindingResult br, Model model) {
+        if (br.hasErrors()) {
+            return "register";
+        } else if (userService.existsByUsername(user.getUsername())) {
+            model.addAttribute("usernameExists", true);
+            return "register";
+        } else if (userService.existsByEmail(user.getEmail())) {
+            model.addAttribute("emailExists", true);
+            return "register";
+        } else if (!user.getPassword().equals(user.getRepeatedPassword())) {
+            model.addAttribute("passwordsNotMatch", true);
+            return "register";
+        }
+        userService.saveUser(user);
+        return "success-registration";
+    }
+}
