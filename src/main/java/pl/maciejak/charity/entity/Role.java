fb831ab90d0ba84604c1d43faba1d232ec1eec78
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-09-20 11:12:01 +0200

    Created Role entity

---
+++ src/main/java/pl/maciejak/charity/entity/Role.java
@@ -0,0 +1,16 @@
+package pl.maciejak.charity.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+}
