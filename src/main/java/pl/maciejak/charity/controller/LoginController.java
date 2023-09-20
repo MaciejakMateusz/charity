cd9b6db4143426ac9b518f83699a3e92785ab10f
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-09-20 12:49:23 +0200

    Created LoginController

--- src/main/java/pl/maciejak/charity/controller/LoginController.java
+++ src/main/java/pl/maciejak/charity/controller/LoginController.java
@@ -1,15 +1,17 @@
 package pl.maciejak.charity.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.maciejak.charity.entity.User;
 
 @Controller
-@RequestMapping("/")
-public class HomeController {
+@RequestMapping("/login")
+public class LoginController {
     @GetMapping
-    public String home() {
-        return "home";
+    public String login(Model model) {
+        model.addAttribute("user", new User());
+        return "login";
     }
-
 }
