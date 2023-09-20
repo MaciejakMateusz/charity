62bf834eaeb9cbe770561456c53e88fc85ca90f9
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-09-20 11:14:18 +0200

    Created validator annotations for User entity: - Email - Username - Password

---
+++ src/main/java/pl/maciejak/charity/annotation/validator/PasswordValidator.java
@@ -0,0 +1,25 @@
+package pl.maciejak.charity.annotation.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.extern.slf4j.Slf4j;
+import pl.maciejak.charity.annotation.Password;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Slf4j
+public class PasswordValidator implements ConstraintValidator<Password, String> {
+
+    private static final Pattern PASSWORD_REGEX =
+            Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^\\w\\s]).{5,60}$");
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
+        if (value == null || value.isBlank()) {
+            return true;
+        }
+        Matcher matcher = PASSWORD_REGEX.matcher(value);
+        return matcher.matches();
+    }
+}
