862b6354a8ea9c2d5c545f416685b1e56ceb30d3
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-09-21 14:34:33 +0200

    Added "categories" model attribute to DonationController

--- src/main/java/pl/maciejak/charity/controller/DonationController.java
+++ src/main/java/pl/maciejak/charity/controller/DonationController.java
@@ -3,13 +3,25 @@ package pl.maciejak.charity.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.maciejak.charity.entity.Category;
 import pl.maciejak.charity.entity.Donation;
+import pl.maciejak.charity.repository.CategoryRepository;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/form")
 public class DonationController {
+
+    private final CategoryRepository categoryRepository;
+
+    public DonationController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
     @GetMapping
     public String form(Model model) {
         model.addAttribute("donation", new Donation());
@@ -19,5 +31,10 @@ public class DonationController {
     @PostMapping
     public String form() {
         return "form/form-confirmation";
+    }
+
+    @ModelAttribute("categories")
+    private List<Category> getCategories() {
+        return categoryRepository.findAll();
     }
 }
