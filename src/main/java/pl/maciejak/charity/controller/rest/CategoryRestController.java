0fdfeefa5f9928c62231bf9b1cfeaf6409606925
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-09-22 14:18:15 +0200

    Added CategoryRestController to fetch categories by ID in app.js

---
+++ src/main/java/pl/maciejak/charity/controller/rest/CategoryRestController.java
@@ -0,0 +1,23 @@
+package pl.maciejak.charity.controller.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.maciejak.charity.entity.Category;
+import pl.maciejak.charity.service.CategoryService;
+
+@RestController
+@RequestMapping("/api/category")
+public class CategoryRestController {
+    private final CategoryService categoryService;
+
+    public CategoryRestController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("/{id}")
+    public Category getDonationById(@PathVariable Integer id) {
+        return categoryService.findById(id);
+    }
+}
