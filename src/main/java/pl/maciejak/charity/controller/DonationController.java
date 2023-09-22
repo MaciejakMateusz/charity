c13cba4cc589f63eca8e079bff0215cbdd66c5ee
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-09-22 09:01:27 +0200

    - injected DonationService into DonationController - expanded POST action in DonationController - changed dependency injection from CategoryRepository to CategoryService

--- src/main/java/pl/maciejak/charity/controller/DonationController.java
+++ src/main/java/pl/maciejak/charity/controller/DonationController.java
@@ -1,14 +1,17 @@
 package pl.maciejak.charity.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.maciejak.charity.entity.Category;
 import pl.maciejak.charity.entity.Donation;
-import pl.maciejak.charity.repository.CategoryRepository;
+import pl.maciejak.charity.service.CategoryService;
+import pl.maciejak.charity.service.DonationService;
 
 import java.util.List;
 
@@ -16,11 +19,14 @@ import java.util.List;
 @RequestMapping("/form")
 public class DonationController {
 
-    private final CategoryRepository categoryRepository;
+    private final CategoryService categoryService;
+    private final DonationService donationService;
 
-    public DonationController(CategoryRepository categoryRepository) {
-        this.categoryRepository = categoryRepository;
+    public DonationController(CategoryService categoryService, DonationService donationService) {
+        this.categoryService = categoryService;
+        this.donationService = donationService;
     }
+
 
     @GetMapping
     public String form(Model model) {
@@ -29,12 +35,16 @@ public class DonationController {
     }
 
     @PostMapping
-    public String form() {
+    public String form(@Valid Donation donation, BindingResult br) {
+        if(br.hasErrors()) {
+            return "form/form";
+        }
+        donationService.save(donation);
         return "form/form-confirmation";
     }
 
     @ModelAttribute("categories")
     private List<Category> getCategories() {
-        return categoryRepository.findAll();
+        return categoryService.findAll();
     }
 }
