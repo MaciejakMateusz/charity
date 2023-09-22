df8f65a708a10196f9d65254b1020f1562a1afb3
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-09-22 18:36:36 +0200

    Updated charity_db.sql

--- src/main/resources/sql/charity_db.sql
+++ src/main/resources/sql/charity_db.sql
@@ -4,10 +4,10 @@ USE charity_donation;
 INSERT INTO charity_donation.roles (id, name) VALUES (1, 'ROLE_USER');
 INSERT INTO charity_donation.roles (id, name) VALUES (2, 'ROLE_ADMIN');
 
-INSERT INTO charity_donation.institutions (name, description) VALUE ('Fundacja "Dbam o Zdrowie"', 'Pomoc dzieciom z ubogich rodzin.');
-INSERT INTO charity_donation.institutions (name, description) VALUE ('Fundacja "A kogo"', 'Pomoc w wybudzaniu dzieci ze śpiączki.');
-INSERT INTO charity_donation.institutions (name, description) VALUE ('Fundacja "Dla dzieci"', 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej.');
-INSERT INTO charity_donation.institutions (name, description) VALUE ('Fundacja "Bez domu"', 'Pomoc dla osób nie posiadających miejsca zamieszkania.');
+INSERT INTO charity_donation.institutions (name, description) VALUE ('"Dbam o Zdrowie"', 'Pomoc dzieciom z ubogich rodzin.');
+INSERT INTO charity_donation.institutions (name, description) VALUE ('"A kogo"', 'Pomoc w wybudzaniu dzieci ze śpiączki.');
+INSERT INTO charity_donation.institutions (name, description) VALUE ('"Dla dzieci"', 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej.');
+INSERT INTO charity_donation.institutions (name, description) VALUE ('"Bez domu"', 'Pomoc dla osób nie posiadających miejsca zamieszkania.');
 
 INSERT INTO charity_donation.categories (name) VALUE ('ubrania, które nadają się do ponownego użycia');
 INSERT INTO charity_donation.categories (name) VALUE ('ubrania do wyrzucenia');
