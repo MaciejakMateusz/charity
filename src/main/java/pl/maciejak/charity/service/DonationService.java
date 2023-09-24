11ff7efed2487c50666371cede750b9005574bac
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-09-24 14:57:46 +0200

    - expanded DonationService save() method to assign submitted donation to currently logged-in user - added donations field to User entity

--- src/main/java/pl/maciejak/charity/service/DonationService.java
+++ src/main/java/pl/maciejak/charity/service/DonationService.java
@@ -1,7 +1,10 @@
 package pl.maciejak.charity.service;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import pl.maciejak.charity.entity.Donation;
+import pl.maciejak.charity.entity.User;
 import pl.maciejak.charity.repository.DonationRepository;
 import pl.maciejak.charity.service.interfaces.DonationServiceInterface;
 
@@ -11,9 +14,11 @@ import java.util.List;
 public class DonationService implements DonationServiceInterface {
 
     private final DonationRepository donationRepository;
+    private final UserService userService;
 
-    public DonationService(DonationRepository donationRepository) {
+    public DonationService(DonationRepository donationRepository, UserService userService) {
         this.donationRepository = donationRepository;
+        this.userService = userService;
     }
 
     @Override
@@ -28,6 +33,15 @@ public class DonationService implements DonationServiceInterface {
 
     @Override
     public void save(Donation donation) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        User user = userService.findByUsername(username);
+
+        List<Donation> donations = user.getDonations();
+        donations.add(donation);
+        user.setDonations(donations);
+
         donationRepository.save(donation);
     }
 
