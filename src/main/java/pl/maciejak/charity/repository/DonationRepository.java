635ad62305443ceb46c4993731f63b8c712811cc
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-09-24 22:30:18 +0200

    Expanded DonationRepository to find donations by user entity

--- src/main/java/pl/maciejak/charity/repository/DonationRepository.java
+++ src/main/java/pl/maciejak/charity/repository/DonationRepository.java
@@ -3,8 +3,13 @@ package pl.maciejak.charity.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import pl.maciejak.charity.entity.Donation;
+import pl.maciejak.charity.entity.User;
+
+import java.util.List;
 
 public interface DonationRepository extends JpaRepository<Donation, Integer> {
     @Query("SELECT SUM(d.quantity) FROM Donation d")
     long sumBags();
+
+    List<Donation> findDonationsByUser(User user);
 }
