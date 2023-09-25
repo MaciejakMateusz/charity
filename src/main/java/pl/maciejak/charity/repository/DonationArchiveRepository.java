1b26ea71cd1722018b55102902eeeb4047517de0
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-09-25 16:34:13 +0200

    Removed DonationArchive and DonationArchiveRepository.java

--- src/main/java/pl/maciejak/charity/repository/DonationArchiveRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.maciejak.charity.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.maciejak.charity.entity.DonationArchive;
-
-public interface DonationArchiveRepository extends JpaRepository<DonationArchive, Long> {
-}
