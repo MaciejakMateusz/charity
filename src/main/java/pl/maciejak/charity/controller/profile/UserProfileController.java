6606d93146ee472952aced1a7982ba932fd9afb3
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-09-26 11:25:29 +0200

    Added missing validation for changing password form

--- src/main/java/pl/maciejak/charity/controller/profile/UserProfileController.java
+++ src/main/java/pl/maciejak/charity/controller/profile/UserProfileController.java
@@ -1,10 +1,12 @@
 package pl.maciejak.charity.controller.profile;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -48,7 +50,10 @@ public class UserProfileController {
     }
 
     @PostMapping("/password")
-    public String password(User user, Model model) {
+    public String password(@Valid User user, BindingResult br, Model model) {
+        if(br.hasErrors()) {
+            return "profile/password/password";
+        }
         if (!user.getPassword().equals(user.getRepeatedPassword())) {
             model.addAttribute("passwordsNotMatch", true);
             return "profile/password/password";
