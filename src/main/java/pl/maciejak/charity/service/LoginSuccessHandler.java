0ec0aabc14bd0265ec5638b1131aeee5b257c9ea
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-09-27 20:47:39 +0200

    Removed unnecessary lines of code that set authentication Object in LoginSuccessHandler

--- src/main/java/pl/maciejak/charity/service/LoginSuccessHandler.java
+++ src/main/java/pl/maciejak/charity/service/LoginSuccessHandler.java
@@ -3,7 +3,6 @@ package pl.maciejak.charity.service;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -15,10 +14,6 @@ public class LoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessH
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException {
-
-        authentication = SecurityContextHolder
-                .getContext()
-                .getAuthentication();
 
         boolean isAdmin = authentication
                 .getAuthorities()
