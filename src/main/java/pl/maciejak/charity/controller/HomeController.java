23e05f5a3e2c132d5b31fd568759fee74c059c29
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-10-01 09:19:18 +0200

    Expanded HomeController to handle POST requests from the contact form

--- src/main/java/pl/maciejak/charity/controller/HomeController.java
+++ src/main/java/pl/maciejak/charity/controller/HomeController.java
@@ -3,8 +3,11 @@ package pl.maciejak.charity.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import pl.maciejak.charity.service.DonationService;
+import pl.maciejak.charity.service.EmailService;
 import pl.maciejak.charity.service.InstitutionService;
 
 @Controller
@@ -13,10 +16,12 @@ public class HomeController {
 
     private final InstitutionService institutionService;
     private final DonationService donationService;
+    private final EmailService emailService;
 
-    public HomeController(InstitutionService institutionService, DonationService donationService) {
+    public HomeController(InstitutionService institutionService, DonationService donationService, EmailService emailService) {
         this.institutionService = institutionService;
         this.donationService = donationService;
+        this.emailService = emailService;
     }
 
     @GetMapping
@@ -25,6 +30,15 @@ public class HomeController {
         model.addAttribute("bagsCount", donationService.bagsCount());
         model.addAttribute("institutions", institutionService.findAll());
         return "home";
+    }
+
+    @PostMapping("/contact-form")
+    public String contact(@RequestParam String name,
+                          @RequestParam String surname,
+                          @RequestParam String email,
+                          @RequestParam String message) {
+        emailService.contactForm(email, "Wiadomość od " + name + " " + surname, message);
+        return "redirect:/";
     }
 
 }
