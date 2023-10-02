6bd6db9f3e504042ff9b2434f63523bff6e4ecc0
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-10-02 20:37:01 +0200

    Expanded TokenRepository

--- src/main/java/pl/maciejak/charity/repository/TokenRepository.java
+++ src/main/java/pl/maciejak/charity/repository/TokenRepository.java
@@ -3,7 +3,12 @@ package pl.maciejak.charity.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.maciejak.charity.entity.Token;
+import pl.maciejak.charity.entity.User;
+
+import java.util.Optional;
 
 @Repository
 public interface TokenRepository extends JpaRepository<Token, Long> {
+    Optional<Token> findTokenByUser(User user);
+    void deleteByUser(User user);
 }
