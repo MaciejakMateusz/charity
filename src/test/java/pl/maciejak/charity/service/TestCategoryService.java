7231c31b2179758f3d18802fa159214a0472bb85
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-10-14 21:08:44 +0200

    Created new test class - TestCategoryService with one test regarding getting category by ID

---
+++ src/test/java/pl/maciejak/charity/service/TestCategoryService.java
@@ -0,0 +1,34 @@
+package pl.maciejak.charity.service;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import pl.maciejak.charity.entity.Category;
+import pl.maciejak.charity.service.interfaces.CategoryServiceInterface;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+@TestPropertySource(locations = "classpath:test.properties")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class TestCategoryService {
+
+    @Autowired
+    private CategoryServiceInterface categoryService;
+
+    @Test
+    public void shouldFindById() {
+        Category category = categoryService.findById(3L);
+        assertEquals("zabawki", category.getName());
+    }
+}
