7f6709b48d166c96a2160c85c8bc98a444aee7ef
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-10-14 21:00:50 +0200

    Filled data-h2.sql with test data

---
+++ src/test/resources/data-h2.sql
@@ -0,0 +1,13 @@
+INSERT INTO roles (id, name) VALUES (1, 'ROLE_USER');
+INSERT INTO roles (id, name) VALUES (2, 'ROLE_ADMIN');
+
+INSERT INTO institutions (name, description) VALUES ('"Dbam o Zdrowie"', 'Pomoc dzieciom z ubogich rodzin.');
+INSERT INTO institutions (name, description) VALUES ('"A kogo"', 'Pomoc w wybudzaniu dzieci ze śpiączki.');
+INSERT INTO institutions (name, description) VALUES ('"Dla dzieci"', 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej.');
+INSERT INTO institutions (name, description) VALUES ('"Bez domu"', 'Pomoc dla osób nie posiadających miejsca zamieszkania.');
+
+INSERT INTO categories (name) VALUES ('ubrania, które nadają się do ponownego użycia');
+INSERT INTO categories (name) VALUES ('ubrania do wyrzucenia');
+INSERT INTO categories (name) VALUES ('zabawki');
+INSERT INTO categories (name) VALUES ('książki');
+INSERT INTO categories (name) VALUES ('inne');
