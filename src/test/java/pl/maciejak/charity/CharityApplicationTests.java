57f39552cbc149545b1d37aa12ff0d16cb630611
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-10-15 12:14:27 +0200

    Updated CharityApplicationTests

--- src/test/java/pl/maciejak/charity/CharityApplicationTests.java
+++ src/test/java/pl/maciejak/charity/CharityApplicationTests.java
@@ -1,9 +1,21 @@
 package pl.maciejak.charity;
 
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+@TestPropertySource(locations = "classpath:test.properties")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class CharityApplicationTests {
 
     @Test
